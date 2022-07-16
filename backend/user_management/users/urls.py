3df1164ac1b08ac797dfcc1be44d5298d3a33e05
Roman Semenenko <semenenkoroma01@gmail.com>
2022-07-16 14:22:42 +0300

    Added account confirmation by email

--- backend/user_management/users/urls.py
+++ backend/user_management/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from users.views import UsersListCreateAPIView
+from users.views import UsersListCreateAPIView, UserActivateView
 
 
 urlpatterns = [
     path('', UsersListCreateAPIView.as_view(), name='user_list_create_api_view'),
+    path('activate/', UserActivateView.as_view(), name='user_activate_view'),
 ]
--- backend/user_management/users/views.py
+++ backend/user_management/users/views.py
@@ -1,4 +1,9 @@
 from rest_framework import generics
+from rest_framework import views
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.tokens import default_token_generator
 from users.models import CustomUser
 from users.serializers import UserListSerializer
 from users.permissions import UserListCreatePermission
@@ -12,3 +17,18 @@ class UsersListCreateAPIView(generics.ListCreateAPIView):
 
     def get_queryset(self):
         return CustomUser.objects.all()
+
+
+class UserActivateView(views.APIView):
+    permission_classes = (
+        AllowAny,
+    )
+
+    def get(self, request):
+        user = CustomUser.objects.filter(id=request.GET.get('user')).first()
+        token = request.GET.get('token')
+        if not default_token_generator.check_token(user, token):
+            return Response(data={'message': 'User not found or token is expired'}, status=status.HTTP_200_OK)
+        user.is_active = True
+        user.save()
+        return Response(data={'message': 'User successfully confirmed'}, status=status.HTTP_200_OK)
