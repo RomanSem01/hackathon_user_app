3116e7f16c3369ce27eee63a6922e0c6347c9559
LeroyGorn <leroygorn@gmail.com>
2022-07-16 11:52:31 +0300

    Added user model

--- backend/user_management/users/models.py
+++ backend/user_management/users/models.py
@@ -1,3 +1,62 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
+    
+    def _create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_staff", True)
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        verbose_name="email",
+        unique=True
+    )
+    username = models.CharField(
+        verbose_name="user",
+        max_length=64,
+        unique=True
+    )
+    created_on = models.DateTimeField(
+        verbose_name="created on",
+        auto_now_add=True
+    )
+
+    is_staff = models.BooleanField(
+        verbose_name="is staff",
+        default=False,
+    )
+
+    is_active = models.BooleanField(
+        verbose_name="is active",
+        default=False,
+    )
+
+    object = CustomUserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'password']
+
+    class Meta:
+        verbose_name = "User",
+        verbose_name_plural = "Users"
