f406d3af3d1f785b208336ac8f62d20c24bee593
LeroyGorn <leroygorn@gmail.com>
2022-07-17 11:55:13 +0300

    Added logout for user

--- backend/user_management/apps/auth_user/urls.py
+++ backend/user_management/apps/auth_user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from apps.auth_user.views import UserLoginView
+from apps.auth_user.views import UserLoginView, UserLogoutView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("login/", UserLoginView.as_view(), name="user_login"),
+    path("logout/", UserLogoutView.as_view(), name="user_login"),
 ]
--- backend/user_management/apps/auth_user/views.py
+++ backend/user_management/apps/auth_user/views.py
@@ -1,6 +1,10 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from apps.auth_user.serializers import CustomObtainTokenSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class UserLoginView(TokenObtainPairView):
@@ -8,3 +12,17 @@ class UserLoginView(TokenObtainPairView):
         AllowAny,
     )
     serializer_class = CustomObtainTokenSerializer
+
+
+class UserLogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
