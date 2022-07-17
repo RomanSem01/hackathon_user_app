8357cbb13dbd3b8044da8057ec8bc06b1a5d0eef
baliubakh <74013378+baliubakh@users.noreply.github.com>
2022-07-17 13:08:23 +0300

    feat: fixed activation link

--- frontend/pages/activation/index.tsx
+++ frontend/pages/activation/index.tsx
@@ -1,26 +1,47 @@
-﻿import { NextPage } from 'next';
+﻿import { GetStaticPropsContext, NextPage } from 'next';
 import { useRouter } from 'next/router';
 import React from 'react';
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+import {
+  dehydrate,
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from 'react-query';
 import { queryKeys } from '../../constants/query-keys.constants';
 import { authService } from '../../services/auth.service';
 import { IActivationData } from '../../types/auth.types';
 import * as Styled from '../../styles/activation.styled';
 
-const Activation: NextPage = () => {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-      },
-    },
-  });
-  const router = useRouter();
+export async function getServerSideProps(context: GetStaticPropsContext) {
+  const user = context.params?.user;
+  const token = context.params?.token;
+  const queryClient = new QueryClient();
 
-  const { user, token } = router.query;
-  const queryString = user && token ? `?user=${user}&token=${token}` : '';
-  const { data }: IActivationData = useQuery(queryKeys.activationLink, () =>
+  const queryString = user && token ? `/?user=${user}&token=${token}` : '/';
+
+  await queryClient.prefetchQuery(queryKeys.activationLink, () =>
     authService.activation(queryString),
+  );
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+}
+
+const Activation: NextPage = () => {
+  const queryClient = new QueryClient();
+  const router = useRouter();
+  const { user, token } = router.query;
+
+  const queryString = user && token ? `/?user=${user}&token=${token}` : '/';
+  const { data }: IActivationData = useQuery(
+    queryKeys.activationLink,
+    () => authService.activation(queryString),
+    {
+      refetchOnWindowFocus: false,
+    },
   );
 
   return (
